8516ee9209e8d83ce3430f59b22c473fb4498def
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-27 18:12:29 +0300

    make rating a property to ensure it is an integer

--- lib/review.py
+++ lib/review.py
@@ -1,8 +1,21 @@
 class Review:
+    all_review=[]
     def __init__(self,customer,restaurant, rating):
        self.customer=customer
        self.restaurant=restaurant
        self.rating=rating
-    def rating(self):
-        return self.rating   
+       Review.add_to_reviews(self)
+    def get_rating(self):
+        return self._rating  
+    def set_rating(self,rating):
+        if type(rating) is int:
+            self._rating = rating
+        else:
+            print('The value must be an integer.')
+    rating = property(get_rating, set_rating) 
+
+    @classmethod
+    def add_to_reviews(cls, review):
+        cls.all_reviews.append(review)
+
     pass
