74c706f30753451847aabad820e1a5eb0b4e4e6d
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-27 12:59:00 +0300

    methods for customer name

--- lib/customers.py
+++ lib/customers.py
@@ -2,7 +2,14 @@ class Customer:
     def __init__(self, given_name, family_name):
         self.given_name=given_name
         self.family_name=family_name
-        
 
+    def given_name(self):
+        return self.given_name     
+    
+    def family_name(self):
+        return self.family_name    
+    
+    def full_name(self):
+        return self.given_name + self.family_name
         
     pass
